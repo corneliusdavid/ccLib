090c2419048f78fd238f00ae2754796a931e105a
corneliusdavid <dev@corneliusconcepts.com>
2015-10-23 12:07:31 -0700

    a utility procedure to find files of a certain pattern and allow the calling method to collect the TSearchRecs of each file

---
+++ uSearchRecList.pas
@@ -0,0 +1,80 @@
+unit uSearchRecList;
+
+interface
+
+uses
+  System.SysUtils, System.IOUtils;
+
+type
+  TPathStatusProc = reference to procedure (const Path: string; var Stop: Boolean);
+  TFileFoundProc = reference to procedure (FileInfo: TSearchRec);
+
+{$REGION 'XMLDoc'}
+/// <summary>
+///   Walk through a directory tree looking for specific files and return them
+///   in callback procedures
+/// </summary>
+/// <param name="Path">
+///   The path to search for files
+/// </param>
+/// <param name="Pattern">
+///   A file pattern to look for
+/// </param>
+/// <param name="Recursive">
+///   True or False - whether to traverse subfolders or not
+/// </param>
+/// <param name="PathStatusProc">
+///   A procedure of type TPathStatusProc for the calling method to define to
+///   get notified every time a sub-folder is recursed into
+/// </param>
+/// <param name="FileFoundProc">
+///   A procedure of type TFileFoundProc called for each found file, giving the
+///   TSearchRec to the calling method.
+/// </param>
+/// <example>
+///   MySearchList := TList&lt;TSearchRec&gt;.Create; <br /><br />
+///   GetSearchRecs(edtPath.Text, edtExt.Text, True, <br />procedure (const
+///   Path: string; var Stop: Boolean) <br />begin <br />
+///   StatusBar1.Panels[0].Text := Path; <br />StatusBar1.Update; <br />
+///   Application.ProcessMessages; <br /><br />Stop := DoStop; <br />end, <br />
+///   procedure (FileInfo: TSearchRec) <br />begin <br />
+///   MySearchList.Add(FileInfo); <br />end);
+/// </example>
+{$ENDREGION}
+procedure GetSearchRecs(const Path, Pattern: string; const Recursive: Boolean; PathStatusProc: TPathStatusProc; FileFoundProc: TFileFoundProc);
+
+
+implementation
+
+procedure GetSearchRecs(const Path, Pattern: string; const Recursive: Boolean; PathStatusProc: TPathStatusProc; FileFoundProc: TFileFoundProc);
+{ adapted from TDirectory.WalkThroughDirectory in System.IOUtils }
+const
+  FCCurrentDir: string = '.';
+  FCParentDir: string = '..';
+var
+  SearchRec: TSearchRec;
+  Stop: Boolean;
+begin
+  if FindFirst(TPath.Combine(Path, '*'), faAnyFile, SearchRec) = 0 then
+    try
+      repeat
+        if SearchRec.Attr and System.SysUtils.faDirectory <> 0 then begin
+          if Assigned(PathStatusProc) then
+            PathStatusProc(Path, Stop);
+          if Stop then
+            Break;
+
+          // if a directory, optionally recurse into it
+          if Recursive and (SearchRec.Name <> FCCurrentDir) and (SearchRec.Name <> FCParentDir) then
+            GetSearchRecs(TPath.Combine(Path, SearchRec.Name), Pattern, Recursive, PathStatusProc, FileFoundProc);
+        end else if TPath.MatchesPattern(SearchRec.Name, Pattern, False) then
+          // found a file matching our pattern, add it to the list
+          if Assigned(FileFoundProc) then
+            FileFoundProc(SearchRec);
+      until FindNext(SearchRec) <> 0;
+    finally
+      FindClose(SearchRec);
+    end;
+end;
+
+end.
