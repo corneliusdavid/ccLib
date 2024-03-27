25ae6b6f268700495ad7bb3f1d1b2560589cd2a4
David Cornelius <dev@corneliusconcepts.com>
2024-03-27 11:26:29 -0700

    demo: fixed unit references

--- Demos/Unit1.pas
+++ Demos/Unit1.pas
@@ -9,7 +9,7 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.ComCtrls,
   {$ELSE}
-  Forms, Messages, SysUtils, ExtCtrls, ComCtrls, Controls, StdCtrls, Classes,
+  Forms, Messages, Dialogs, SysUtils, ExtCtrls, ComCtrls, Controls, StdCtrls, Classes,
   {$ENDIF}
   CloseApplication;
 
